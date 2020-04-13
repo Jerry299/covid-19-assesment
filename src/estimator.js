7056f8af81ee46af4c0707c43d96adbc7bd2ab77
Jerry299 <josephjeremiah564@yahoo.com>
2020-04-13 10:03:58 +0100

    removed console.logs

--- src/estimator.js
+++ src/estimator.js
@@ -110,7 +110,7 @@ const covid19ImpactEstimator = (data) => {
   } */
 // your severe case estimation
 
-const data = {
+/* const data = {
   region: {
     name: 'Africa',
     avgAge: 23,
@@ -125,6 +125,6 @@ const data = {
 };
 
 const res1 = covid19ImpactEstimator(data);
-console.log(res1);
+console.log(res1); */
 
-// export default covid19ImpactEstimator;
+export default covid19ImpactEstimator;
